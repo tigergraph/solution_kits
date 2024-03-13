22096107e5bce06e4c77f74fb4a3d4e6ea56ac3e
Lin Yu <lin.yu@tigergraph.com>
2024-03-13 16:10:38 +0800

    add meta

--- financial_crime/transaction_fraud/transaction_fraud/install_queries.sh
+++ financial_crime/transaction_fraud/transaction_fraud/install_queries.sh
@@ -8,8 +8,12 @@ QUERY_DIR="./queries"
 
 # Iterate over each .gsql file in the directory
 for file in "$QUERY_DIR"/*.gsql; do
-    echo "Running $file..."
-    gsql -g "$GRAPH_NAME" "$file"
+# Execute each query in the file
+    # skip install_query.gsql
+    if [ $file != "./queries/install_query.gsql" ]; then 
+        echo "Running $file..."
+        gsql -g "$GRAPH_NAME" "$file"
+    fi
 done
 
 echo "All scripts have been executed."
